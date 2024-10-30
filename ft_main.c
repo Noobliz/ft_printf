4e39a36dbfe2929f22a4e3cf0ef0ecd752031c8d
Lise <165786617+Noobliz@users.noreply.github.com>
2024-10-30 09:50:53 +0100

    Add files via upload

--- ft_main.c
+++ ft_main.c
@@ -10,5 +10,20 @@ int	main(void)
 
 	printf("%d\n", printf("%s\n", hello));
 	ft_printf("%d\n", ft_printf("%s\n", hello));
+
+	/*
+	int	count;
+	count = 0;
+	ft_putbin(2, &count);
+    ft_putchar('\n', &count);
+    count = 0;
+    ft_putbin(5, &count);
+    ft_putchar('\n', &count);
+    count = 0;
+    ft_putbin(10, &count);
+    ft_putchar('\n', &count);
+    count = 0;
+    ft_putbin(255, &count);
+    ft_putchar('\n', &count);*/
 	return (0);
 }
